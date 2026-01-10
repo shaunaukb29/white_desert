7ad38050d00b38bda1804f29b441c5ea187cd444
shaunaukb29 <shaunak.basu29@gmail.com>
2026-01-10 14:42:40 +0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,12 +1,37 @@
+"use client";
+
 export default function Home() {
   return (
-    <main style={{ margin: 0, padding: 0, height: "100vh", width: "100vw" }}>
-      <iframe
-        src="https://huggingface.co/spaces/CastBison/golden_seas"
-        style={{ border: "none", width: "100%", height: "100%" }}
-        title="Golden Seas App"
-      />
+    <main
+      style={{
+        margin: 0,
+        padding: 0,
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#0f1117",
+      }}
+    >
+      <div
+        style={{
+          width: "100%",
+          maxWidth: "1400px",
+          height: "100%",
+          borderRadius: "20px",
+          overflow: "hidden",
+          boxShadow: "0 12px 32px rgba(0,0,0,0.35)",
+        }}
+      >
+        <gradio-app src="https://huggingface.co/spaces/CastBison/golden_seas"></gradio-app>
+      </div>
+
+      {/* Load Gradio JS */}
+      <script
+        type="module"
+        src="https://gradio.s3-us-west-2.amazonaws.com/5.38.0/gradio.js"
+      ></script>
     </main>
   );
 }
-
