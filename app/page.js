f82f1e5fec95a2a32c069a8f98043c7a9d8df313
shaunaukb29 <shaunak.basu29@gmail.com>
2026-01-10 14:35:37 +0400

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main style={{ margin: 0, padding: 0, height: "100vh", width: "100vw" }}>
+      <iframe
+        src="https://huggingface.co/spaces/CastBison/golden_seas"
+        style={{ border: "none", width: "100%", height: "100%" }}
+        title="Golden Seas App"
+      />
+    </main>
+  );
+}
+
